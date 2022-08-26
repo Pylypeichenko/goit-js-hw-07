59cb56c11c2340ff82b1cea87e3f44e35a7f4b56
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-08-26 14:48:35 +0300

    Add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+
+const createGalleryMarkup = galleryItems
+  .map(
+    (item) => `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      title="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+galleryRef.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery a");
+  gallery.on("show.simplelightbox", function () {
+    gallery.captionDelay = 250;
+  });
+});
+
+//  <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>;
+
+// {/* <a class="gallery__item" href="large-image.jpg">
+//   <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+// </a>; */}
 
 console.log(galleryItems);
